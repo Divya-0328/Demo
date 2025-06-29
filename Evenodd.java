125d530cf3e329989581bfaf82b79245a2a06524
Divya Bolnedi <divyabolnedi@gmail.com>
2025-06-29 13:37:58 +0530

    first commit

---
+++ Evenodd.java
@@ -0,0 +1,14 @@
+class Evenodd{
+    public static void main(String args[])
+    {
+        int a=10;
+        if(a%2==0)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
